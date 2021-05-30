b8f410f2add90ef8691c233d31c702f6710577e3
roberto palacios <robpalacios1@hotmail.com>
2021-05-30 12:45:25 -0500

    fix code by Betty

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -8,29 +8,29 @@
 
 int is_palindrome(listint_t **head)
 {
-  listint_t *tmp;
-  int i, size = 0;
-  listint_t *array[3000];
+	listint_t *tmp;
+	int i, size = 0;
+	listint_t *array[3000];
 
-  if (!head)
-    return (0);
-  if (!(*head) || !(*head)->next)
-    return (1);
-  for (i = 0; i < 3000; i++)
-    array[i] = NULL;
-  tmp = *head;
-  while (tmp)
-  {
-    array[size] = tmp;
-    size++;
-    tmp = tmp->next;
-  }
-  for (i = 0; i < (size / 2); i++)
-  {
-    if (array[i]->n != array[size - 1 - i]->n)
-    {
-      return (0);
-    }
-  }
-  return (1);
+	if (!head)
+		return (0);
+	if (!(*head) || !(*head)->next)
+		return (1);
+	for (i = 0; i < 3000; i++)
+		array[i] = NULL;
+	tmp = *head;
+	while (tmp)
+	{
+		array[size] = tmp;
+		size++;
+		tmp = tmp->next;
+	}
+	for (i = 0; i < (size / 2); i++)
+	{
+		if (array[i]->n != array[size - 1 - i]->n)
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
