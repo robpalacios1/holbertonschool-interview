435ba47d9406e006d342581e28403a22ceff30c5
roberto palacios <robpalacios1@hotmail.com>
2021-08-05 20:53:56 -0500

    fix print_skiplist.c file

--- 0x0E-linear_skip/skiplist/print_skiplist.c
+++ 0x0E-linear_skip/skiplist/print_skiplist.c
@@ -14,14 +14,14 @@ void print_skiplist(const skiplist_t *list)
 	const skiplist_t *node;
 
 	printf("List :\n");
-	for (node = list; node; node = node->next)
+	for (node = list; node; node = node -> next)
 	{
-		printf("Index[%lu] = [%d]\n", node->index, node->n);
+		printf("Index[%lu] = [%d]\n", node -> index, node -> n);
 	}
 	printf("\nExpress lane :\n");
-	for (node = list; node; node = node->express)
+	for (node = list; node; node = node -> express)
 	{
-		printf("Index[%lu] = [%d]\n", node->index, node->n);
+		printf("Index[%lu] = [%d]\n", node -> index, node -> n);
 	}
 	printf("\n");
 }
